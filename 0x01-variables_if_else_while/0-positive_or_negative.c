3b415add03b517957dc5e9e520df3caffb9c5c05
Samuel Dike <samueldikeomaka@gmail.com>
2022-07-02 00:18:28 -0700

    Yet another Positive or negative fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,17 +4,12 @@
 
 #include <stdio.h>
 
-
-
 /**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
  *
- *  * main - assigns a random number to int n everytime
- *
- *   * it executes, and prints it
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
