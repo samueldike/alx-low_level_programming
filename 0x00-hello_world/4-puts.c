d9e85d802ec8284e8a5bf9d253227ff2ecda3d3d
Samuel Dike <samueldikeomaka@gmail.com>
2022-07-04 13:04:00 -0700

    modifed puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
 nclude <stdio.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
-
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+ *main - Entry point
+ *Return: always 0
+ */
+int main(void){	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
