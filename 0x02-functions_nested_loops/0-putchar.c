b647de517bd728b187a0b498939605e8ac658cc0
Samuel Dike <samueldikeomaka@gmail.com>
2022-07-05 22:04:53 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - function to print
  * Return: Always returns 0
  */
 int main(void)
 {
-	putchar("");
-	_putchar("p");
+	putchar('');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar("\n");
+	_putchar('\n');
+
 	return (0);	
 }
