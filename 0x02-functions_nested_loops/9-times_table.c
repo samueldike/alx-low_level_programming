31c2eed757253d95865189ade7e57c35d4fffe87
Samuel Dike <samueldikeomaka@gmail.com>
2022-07-08 03:35:15 -0700

    Fixed betty error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,10 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			int prod = j * i;
-			if (j == 0){
+			if (j == 0)
+			{
 				_putchar('0');
-			} 
+			}
 			else if (prod <= 9)
 			{
 				_putchar(',');
