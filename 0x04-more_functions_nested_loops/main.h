12150fb4f21c708e6165cc9a7240e6fc09bdd19b
Samuel Dike <samueldikeomaka@gmail.com>
2022-07-13 05:16:27 -0700

    Fixed main.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
-ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);
 int _isupper(int c);
-int _isdigit(int c);
+int isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
