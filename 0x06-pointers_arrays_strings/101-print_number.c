464626237f1a412ad25f33a0eedd845ec4dd9e67
Samuel Dike <samueldikeomaka@gmail.com>
2022-07-13 23:48:27 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,17 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 /**
-  * main - Prints magic
-  * Return: 0
-  */
-int main(void)
+  *print_number - prints an integer.
+  *@n: input integer.
+  * Return: no return.
+ */
+void print_number(int n)
 {
-	int n;
-	int a[5];
-	int *p;
+	unsigned int m, d, count;
 
-	a[2] = 1024;
-	p = &n;
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
 
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
