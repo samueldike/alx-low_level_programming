a08f12d2006e07f36e5f4b9ae054a5108ee72891
Samuel Dike <samueldikeomaka@gmail.com>
2022-07-28 01:01:09 -0700

    0-isupper.c fixed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,10 @@
 #define _MAIN_H_
 
 int _isupper(int c);
-int isdigit(int c);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int putchar(char);
+int _putchar(char);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
